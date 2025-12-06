456ea4bb71a28834927fa268175d87370f5c7f6b
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2025-12-06 12:47:04 +0100

    add header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,28 @@
+import "./Header.css";
+import appLogo from "./assets/logo.png";
+
 function Header() {
   return (
-    <header>
-      <h1>Welcome to PsicologiaLive</h1>
+    <header className="header">
+      <div className="logo">
+        <img src={appLogo} style={{ width: "74px", height: "40px" }}></img>
+      </div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="#">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">Servicios</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">Fundación</a>
+          </li>
+          <li className="nav-item">
+            <a href="#">Contacto</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
