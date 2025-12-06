f38e4071678f8b44a27a9947b95801aa2a7882ac
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2025-12-06 13:14:56 +0100

    social media icons

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,24 +5,50 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <img src={appLogo} style={{ width: "74px", height: "40px" }}></img>
+        <img src={appLogo}></img>
       </div>
       <nav className="nav">
         <ul className="nav-list">
-          <li className="nav-item">
+          <li className="nav-item real">
             <a href="#">Inicio</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item real">
             <a href="#">Servicios</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item real">
             <a href="#">Fundación</a>
           </li>
-          <li className="nav-item">
+          <li className="nav-item real">
             <a href="#">Contacto</a>
           </li>
         </ul>
       </nav>
+      <div className="social-buttons nav-item">
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fa fa-facebook"
+        ></a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fa fa-instagram"
+        ></a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fa fa-linkedin"
+        ></a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fa fa-whatsapp"
+        ></a>
+      </div>
     </header>
   );
 }
