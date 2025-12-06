e78a7befd66bb933ae2b93c65e726bd095184e71
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2025-12-06 13:36:22 +0100

    button cita

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ function Home() {
   return (
     <div className="home-container">
       <h1 className="home-title">Te acompañamos en tu proceso psicológico</h1>
+      <button className="home-button">Quiero una cita</button>
     </div>
   );
 }
